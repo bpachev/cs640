f4d474a1a07e89fcd48abeb34edd55c9bbf63189
Benjamin Pachev <benjaminpachev@gmail.com>
2019-10-03 13:28:33 -0600

    Can test XGBoost classifier

--- classifier.py
+++ classifier.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser, FileType
 from sklearn.svm import LinearSVC, SVC
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import StandardScaler
+import xgboost as xgb
 
 def get_dataset(feature_files):
 	train_mask, labels = None, None
@@ -21,13 +22,7 @@ def get_dataset(feature_files):
 	test_mask = ~train_mask
 	return final_mat[train_mask], labels[train_mask], final_mat[test_mask], labels[test_mask]
 
-if __name__ == "__main__":
-	parser = ArgumentParser()
-	parser.add_argument("inputfiles", nargs="+", type=FileType('r'))
-	#For now, do a simple train-test split
-
-	args = parser.parse_args()
-	
+def svc(args):
 	trainx, trainy, testx, testy = get_dataset(args.inputfiles)
 	model = LinearSVC(C=.005)
 #	scaler = StandardScaler()
@@ -36,5 +31,44 @@ if __name__ == "__main__":
 	model.fit(trainx, trainy)
 	print model.score(testx, testy), "APR"
 	print confusion_matrix(testy, model.predict(testx))
+	
+
+def test_xgb(args):
+	trainx, trainy, testx, testy = get_dataset(args.inputfiles)
+	trainy -= 1
+	testy -= 1
+	dtrain = xgb.DMatrix(trainx, trainy)
+	dtest = xgb.DMatrix(testx, testy)
+	
+	params = {"objective":"multi:softmax", "num_class":10, 'eta':.5, 'colsample_bytree':.9}
+	bst = xgb.train(params, dtrain, num_boost_round=500)
+#	model = LinearSVC(C=.005)
+#	scaler = StandardScaler()
+#	trainx = scaler.fit_transform(trainx)
+#	testx = scaler.transform(testx)
+	pred = bst.predict(dtest)
+#	print model.score(testx, testy), "APR"
+	print np.mean(pred==testy)
+	print confusion_matrix(testy, pred)
+
+def test_sklearn_xgb(args):
+	trainx, trainy, testx, testy = get_dataset(args.inputfiles)
+	trainy -= 1
+	testy -= 1
+	from sklearn.multiclass import OneVsRestClassifier
+	model = OneVsRestClassifier(xgb.XGBClassifier(njobs=-1, eta=.5, num_boost_round=200))
+	model.fit(trainx, trainy)
+	pred = model.predict(testx)
+	print np.mean(pred==testy)
+	print confusion_matrix(testy, pred)
+
+
+if __name__ == "__main__":
+	parser = ArgumentParser()
+	parser.add_argument("inputfiles", nargs="+", type=FileType('r'))
+	#For now, do a simple train-test split
+
+	args = parser.parse_args()
+	test_sklearn_xgb(args)
 #	print trainx, trainy
 #	print testx, testy
